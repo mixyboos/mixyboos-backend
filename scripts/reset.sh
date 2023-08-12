2af914b837e585fd58e8d1eb9b866cce153bd486
Fergal Moran <fergal.moran@gmail.com>
2023-08-12 21:00:38 +0100

    Update scripts

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 rm -rf /srv/dev/mixyboos/mixyboos-backend/mixyboos-api/Migrations/*
+#read -p "Press enter to continue"
+
 cd /srv/dev/mixyboos/mixyboos-backend/mixyboos-api || exit
 
 dotnet ef migrations add "Initial"
 dotnet ef database update
 
-psql $DBNAME -c \
-  "SELECT table_schema, table_name FROM information_schema.tables WHERE table_schema IN ('auth', 'public') ORDER BY table_schema, table_name"
-
+#psql $DBNAME -c \
+#  "SELECT table_schema, table_name FROM information_schema.tables WHERE table_schema IN ('auth', 'public') ORDER BY table_schema, table_name"
+#
--- scripts/reset_local.sh
+++ scripts/reset_local.sh
@@ -1,12 +1,40 @@
 #!/usr/bin/env bash
 
-export PGUSER=postgres
-export PGPASSWORD=hackme
-export PGHOST=localhost
-export DBNAME=mixyboos
-export ASPNETCORE_Environment=Development
 
-dropdb -f --if-exists ${DBNAME}
-createdb ${DBNAME}
+reset_pg() {
+  export PGUSER=postgres
+  export PGPASSWORD=hackme
+  export PGHOST=localhost
+  export DBNAME=mixyboos
+  export ASPNETCORE_Environment=Development
+  
+  dropdb -f --if-exists ${DBNAME}
+  createdb ${DBNAME}
+}
 
+reset_mssql() {
+  #!/usr/bin/env bash
+  source $HOME/.prv/env
+  
+  HOST=localhost
+  USER=sa
+  PASSWORD=$MSSQLPASSWORD
+  
+  echo "Closing db connections"
+  sqlcmd \
+      -S $HOST \
+      -U $USER \
+      -P $PASSWORD \
+      -d master \
+      -i ./scripts/sql/drop.sql
+
+  echo "Creating dev db"
+  sqlcmd \
+      -S $HOST \
+      -d master \
+      -U $USER \
+      -P $PASSWORD \
+      -i ./scripts/sql/create_db.sql
+}
+reset_mssql
 ./scripts/reset.sh
