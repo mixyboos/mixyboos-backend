23bd98e89054dcfa46ff3895075471abc06d4a3a
Fergal Moran <fergal.moran@gmail.com>
2025-03-07 17:07:53 +0000

    Improve logging in CheckAudioIsProcessed

--- mixyboos-api/Services/Jobs/CheckAudioIsProcessedJob.cs
+++ mixyboos-api/Services/Jobs/CheckAudioIsProcessedJob.cs
@@ -1,10 +1,12 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using MixyBoos.Api.Controllers.Hubs;
 using MixyBoos.Api.Data;
 using Quartz;
@@ -15,32 +17,41 @@ public class CheckAudioIsProcessedJob : IJob {
   private readonly IConfiguration _config;
   private readonly MixyBoosContext _context;
   private readonly IHttpClientFactory _httpClientFactory;
+  private readonly ILogger<CheckAudioIsProcessedJob> _logger;
   private readonly IHubContext<LiveHub> _hub;
 
   public CheckAudioIsProcessedJob(MixyBoosContext context, IHubContext<LiveHub> hub,
-    IConfiguration config, IHttpClientFactory httpClientFactory) {
+    IConfiguration config, IHttpClientFactory httpClientFactory, ILogger<CheckAudioIsProcessedJob> logger) {
     _context = context;
     _hub = hub;
     _config = config;
     _httpClientFactory = httpClientFactory;
+    _logger = logger;
   }
 
   public async Task Execute(IJobExecutionContext context) {
-    var unprocessed = await _context.Mixes.Where(m => !m.IsProcessed)
-      .Include(m => m.User)
-      .ToListAsync();
+    _logger.LogDebug($"Checking for unprocessed audio");
+    try {
+      var unprocessed = await _context.Mixes.Where(m => !m.IsProcessed)
+        .Include(m => m.User)
+        .ToListAsync();
 
-    foreach (var mix in unprocessed) {
-      var manifestFile = Path.Combine(_config["AudioProcessing:OutputDir"],
-        mix.Id.ToString(),
-        $"{mix.Id}.m3u8");
-      if (File.Exists(manifestFile)) {
-        mix.IsProcessed = true;
+      _logger.LogDebug("{UnprocessedCount} unprocessed audio found", unprocessed.Count);
+      foreach (var mix in unprocessed) {
+        _logger.LogDebug("Reprocessing {mix.Id}", mix.User.Id);
+        var manifestFile = Path.Combine(_config["AudioProcessing:OutputDir"],
+          mix.Id.ToString(),
+          $"{mix.Id}.m3u8");
+        if (File.Exists(manifestFile)) {
+          mix.IsProcessed = true;
+        }
+
+        await _hub.Clients.User(mix.User.Email).SendAsync("ConversionFinished", mix.Id);
       }
 
-      await _hub.Clients.User(mix.User.Email).SendAsync("ConversionFinished", mix.Id);
+      await _context.SaveChangesAsync();
+    } catch (Exception e) {
+      _logger.LogError(e, "An error occured while checking for audio");
     }
-
-    await _context.SaveChangesAsync();
   }
 }
